61f2f4f2bf121c1f42ace5aaa44cac3a3a5b33a6
Seth NeJame <sethnejame@gmail.com>
2019-06-03 09:02:34 +0200

    Adding sad path code to Fizz Buzz Exercise

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,13 +1,17 @@
 # Fizz Buzz method checks if numbers are divisible by 15, 5 or 3
 def fizz_buzz(number)
-    if mod_it(number, 15) 
-        "FizzBuzz!"
-    elsif mod_it(number, 5)
-        "Buzz!"
-    elsif mod_it(number, 3)
-        "Fizz!"
+    if (number.is_a? Integer) && (number > 0) # make sure the number is an integer greater than zero
+        if mod_it(number, 15) 
+            "FizzBuzz!"
+        elsif mod_it(number, 5)
+            "Buzz!"
+        elsif mod_it(number, 3)
+         "Fizz!"
+        else
+            number
+        end
     else
-        number
+        puts "Please enter a number greater than zero."
     end
 end
 
